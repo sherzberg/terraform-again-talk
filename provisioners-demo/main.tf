dfb4ff00d70fe4b4b3c9aaea67cd6d29fed979b0
Spencer Herzberg <spencer.herzberg@gmail.com>
2017-04-26 14:44:39 -0500

    provisioners-demo: fmt

--- provisioners-demo/main.tf
+++ provisioners-demo/main.tf
@@ -1,16 +1,13 @@
-
 variable "version" {
-    default = 2
+  default = 1
 }
 
 resource "null_resource" "current_date" {
+  provisioner "local-exec" {
+    command = "echo $(date) > current.date.${var.version}"
+  }
 
-    provisioner "local-exec" {
-        command = "echo $(date) > current.date.${var.version}"
-    }
-
-    triggers {
-        VERSION = "${var.version}"
-    }
-
+  triggers {
+    VERSION = "${var.version}"
+  }
 }
